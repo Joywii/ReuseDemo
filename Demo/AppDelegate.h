aad9f503c1c7dd38bba99be6c905bc07aad03d8b
zhongweitao <weitaozhong@sohu-inc.com>
2014-09-02 14:52:48 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by joywii on 14-9-2.
+//  Copyright (c) 2014年 sohu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
